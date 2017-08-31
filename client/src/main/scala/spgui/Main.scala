829a168e21f838b8903f8ef7a8be185497d8b353
aleastChs <aleast@student.chalmers.se>
2017-08-31 20:07:00 +0200

    commented main from SP

--- client/src/main/scala/spgui/Main.scala
+++ client/src/main/scala/spgui/Main.scala
@@ -3,11 +3,9 @@
 //import scala.scalajs.js.JSApp
 //import japgolly.scalajs.react._
 //import org.scalajs.dom.document
+//import scala.scalajs.js.annotation.JSExport
 //
-//import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
-//
-//@JSExport("SPAMain")
-//object SPAMain extends JSApp {
+//object Main extends JSApp {
 //  @JSExport
 //  override def main(): Unit = {
 //    Layout().renderIntoDOM(document.getElementById("spgui-root"))
