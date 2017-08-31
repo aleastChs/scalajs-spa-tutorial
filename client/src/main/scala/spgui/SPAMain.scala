eeee277066a1cfb0c0aa2c0ccbe490fc2d2e41af
aleastChs <aleast@student.chalmers.se>
2017-08-31 15:50:00 +0200

    Commented SPAMain in spgui

--- client/src/main/scala/spgui/SPAMain.scala
+++ client/src/main/scala/spgui/SPAMain.scala
@@ -1,15 +1,15 @@
-package spgui
-
-import scala.scalajs.js.JSApp
-import japgolly.scalajs.react._
-import org.scalajs.dom.document
-
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
-
-@JSExport("SPAMain")
-object SPAMain extends JSApp {
-  @JSExport
-  override def main(): Unit = {
-    Layout().renderIntoDOM(document.getElementById("spgui-root"))
-  }
-}
+//package spgui
+//
+//import scala.scalajs.js.JSApp
+//import japgolly.scalajs.react._
+//import org.scalajs.dom.document
+//
+//import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+//
+//@JSExport("SPAMain")
+//object SPAMain extends JSApp {
+//  @JSExport
+//  override def main(): Unit = {
+//    Layout().renderIntoDOM(document.getElementById("spgui-root"))
+//  }
+//}
