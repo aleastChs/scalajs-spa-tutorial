63045a351778d71957a9b42166074624a0dd4d90
aleastChs <aleast@student.chalmers.se>
2017-08-31 12:28:19 +0200

    Added resources from SP, Added spdomain from SP and spgui from SP

---
+++ client/src/main/scala/spgui/SPAMain.scala
@@ -0,0 +1,15 @@
+package spgui
+
+import scala.scalajs.js.JSApp
+import japgolly.scalajs.react._
+import org.scalajs.dom.document
+
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
+@JSExportTopLevel("SPAMain")
+object SPAMain extends JSApp {
+  @JSExport
+  override def main(): Unit = {
+    Layout().renderIntoDOM(document.getElementById("spgui-root"))
+  }
+}
