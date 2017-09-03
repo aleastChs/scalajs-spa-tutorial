94c3ace19e191e37ce468081cb45730539c34805
aleastChs <aleast@student.chalmers.se>
2017-09-03 10:36:58 +0200

    Deleted some backend case classes

--- client/src/main/scala/spgui/circuit/SPGUICircuit.scala
+++ client/src/main/scala/spgui/circuit/SPGUICircuit.scala
@@ -9,12 +9,10 @@ import spgui.theming.Theming.Theme
 import spgui.dashboard.Dashboard
 
 object SPGUICircuit extends Circuit[SPGUIModel] with ReactConnector[SPGUIModel] {
-  def initialModel = BrowserStorage.load.getOrElse(InitialState())
+  def initialModel = InitialState()
   val actionHandler = composeHandlers(
     new DashboardHandler(zoomRW(_.openWidgets)((m,v) => m.copy(openWidgets = v))),
-    new GlobalStateHandler(zoomRW(_.globalState)((m, v) => m.copy(globalState = v))),
-    new SettingsHandler(zoomRW(_.settings)((m, v) => m.copy(settings = v))),
-    new WidgetDataHandler(zoomRW(_.widgetData)((m,v) => m.copy(widgetData = v)))
+    new SettingsHandler(zoomRW(_.settings)((m, v) => m.copy(settings = v)))
   )
   // store state upon any model change
   subscribe(zoomRW(myM => myM)((m,v) => v))(m => BrowserStorage.store(m.value))
@@ -99,24 +97,6 @@ class DashboardHandler[M](modelRW: ModelRW[M, OpenWidgets]) extends ActionHandle
   }
 }
 
-class GlobalStateHandler[M](modelRW: ModelRW[M, GlobalState]) extends ActionHandler(modelRW) {
-  override def handle = {
-    case UpdateGlobalState(state) =>
-      updated(state)
-    case UpdateGlobalAttributes(key, v) =>
-      val attr = value.attributes + (key->v)
-      updated(value.copy(attributes = attr))
-  }
-}
-
-class WidgetDataHandler[M](modelRW: ModelRW[M, WidgetData]) extends ActionHandler(modelRW) {
-  override def handle = {
-    case UpdateWidgetData(id, d) =>
-      val updW = value.xs + (id -> d)
-      updated(WidgetData(updW))
-  }
-}
-
 class SettingsHandler[M](modelRW: ModelRW[M, Settings]) extends ActionHandler(modelRW) {
   override def handle = {
     case SetTheme(newTheme) => {
@@ -133,43 +113,8 @@ class SettingsHandler[M](modelRW: ModelRW[M, Settings]) extends ActionHandler(mo
 }
 
 object BrowserStorage {
-  import sp.domain._
-  import sp.domain.Logic._
-  import JsonifyUIState._
   val namespace = "SPGUIState"
-  def store(spGUIState: SPGUIModel) = LocalStorage(namespace) = SPValue(spGUIState).toJson
-  def load: Option[SPGUIModel] =
-    LocalStorage(namespace).flatMap(x => SPAttributes.fromJsonGetAs[SPGUIModel](x))
+  def store(spGUIState: SPGUIModel) = println("BrowserStorage store" + spGUIState)
+  def load(): Unit = println("BrowserStorage load")
 }
 
-object JsonifyUIState {
-  import sp.domain._
-  import Logic._
-  import play.api.libs.json._
-
-  implicit val fTheme: JSFormat[Theme] = deriveFormatSimple[Theme]
-  implicit val fSettings: JSFormat[Settings] = deriveFormatSimple[Settings]
-  implicit val fWidgetData: JSFormat[WidgetData] = deriveFormatSimple[WidgetData]
-  implicit val fWidgetLayout: JSFormat[WidgetLayout] = deriveFormatSimple[WidgetLayout]
-  implicit val fOpenWidget: JSFormat[OpenWidget] = deriveFormatSimple[OpenWidget]
-
-  implicit lazy val openWidgetMapReads: JSReads[Map[ID, OpenWidget]] = new JSReads[Map[ID, OpenWidget]] {
-    override def reads(json: JsValue): JsResult[Map[ID, OpenWidget]] = {
-      json.validate[Map[String, SPValue]].map{xs =>
-        def isCorrect(k: String, v: SPValue) = ID.isID(k) && v.to[OpenWidget].isSuccess
-        xs.collect{case (k, v) if isCorrect(k, v) => ID.makeID(k).get -> v.to[OpenWidget].get}}
-    }
-  }
-  implicit lazy val openWidgetMapWrites: JSWrites[Map[ID, OpenWidget]] = new OWrites[Map[ID, OpenWidget]] {
-    override def writes(xs: Map[ID, OpenWidget]): JsObject = {
-      val toFixedMap = xs.map{case (k, v) => k.toString -> SPValue(v)}
-      JsObject(toFixedMap)
-    }
-  }
-
-
-  implicit val fOpenWidgets: JSFormat[OpenWidgets] = deriveFormatSimple[OpenWidgets]
-  implicit val fGlobalState: JSFormat[GlobalState] = deriveFormatSimple[GlobalState]
-  implicit val fSPGUIModel: JSFormat[SPGUIModel] = deriveFormatSimple[SPGUIModel]
-
-}
--- client/src/main/scala/spgui/circuit/SPGUIModel.scala
+++ client/src/main/scala/spgui/circuit/SPGUIModel.scala
@@ -2,28 +2,18 @@ package spgui.circuit
 
 import diode._
 import java.util.UUID
-import sp.domain.SPValue
 import spgui.theming.Theming.Theme
 
 // state
 case class SPGUIModel(
   openWidgets: OpenWidgets = OpenWidgets(),
-  globalState: GlobalState = GlobalState(),
   widgetData: WidgetData = WidgetData(Map()),
   settings: Settings = Settings()
 )
 case class OpenWidgets(xs: Map[UUID, OpenWidget] = Map())
 case class OpenWidget(id: UUID, layout: WidgetLayout, widgetType: String)
 case class WidgetLayout(x: Int, y: Int, w: Int, h: Int, collapsedHeight: Int = 1)
-
-case class GlobalState(
-  currentModel: Option[UUID] = None,
-  selectedItems: List[UUID] = List(),
-  userID: Option[UUID] = None,
-  clientID: UUID = UUID.randomUUID(),
-  attributes: Map[String, SPValue] = Map()
-)
-case class WidgetData(xs: Map[UUID, SPValue])
+case class WidgetData(xs: Map[UUID, Unit])
 
 case class Settings(
   theme: Theme = Theme(),
@@ -38,12 +28,10 @@ case class AddWidget(widgetType: String, width: Int = 2, height: Int = 2, id: UU
 case class CloseWidget(id: UUID) extends Action
 case class CollapseWidgetToggle(id: UUID) extends Action
 case object CloseAllWidgets extends Action
-case class UpdateWidgetData(id: UUID, data: SPValue) extends Action
+case class UpdateWidgetData(id: UUID, data: String) extends Action
 case class UpdateLayout(id: UUID, newLayout: WidgetLayout) extends Action
 case class SetLayout(layout: Map[UUID, WidgetLayout]) extends Action
-case class UpdateGlobalState(state: GlobalState) extends Action
 case class SetTheme(theme: Theme) extends Action
-case class UpdateGlobalAttributes(key: String, value: SPValue) extends Action
 case object ToggleHeaders extends Action
 
 // used when failing to retrieve a state from browser storage
