fc6245b37801eb88a980b352a1c655871cb1fd33
aleastChs <aleast@student.chalmers.se>
2017-09-03 10:37:35 +0200

    Deleted backend calls

--- client/src/main/scala/spgui/SPWidget.scala
+++ client/src/main/scala/spgui/SPWidget.scala
@@ -5,25 +5,20 @@ import japgolly.scalajs.react.vdom.html_<^._
 
 import scala.util.Try
 import spgui.circuit._
-import sp.domain._
 import java.util.UUID
 
 
-case class SPWidgetBase(id: UUID, frontEndState: GlobalState) {
+case class SPWidgetBase(id: UUID) {
 
-  def updateWidgetData(data: SPValue): Unit = {
+  def updateWidgetData(data: String): Unit = {
     SPGUICircuit.dispatch(UpdateWidgetData(id, data))
   }
 
   def getWidgetData = {
-    SPGUICircuit.zoom(_.widgetData.xs.get(id)).value.getOrElse(SPValue.empty)
+    SPGUICircuit.zoom(_.widgetData.xs.get(id)).value.getOrElse("")
   }
 
-  def updateGlobalState(state: GlobalState): Unit = {
-    SPGUICircuit.dispatch(UpdateGlobalState(state))
-  }
-
-  def openNewWidget(widgetType: String, initialData: SPValue = SPValue.empty) = {
+  def openNewWidget(widgetType: String, initialData: String = "") = {
     val w = AddWidget(widgetType = widgetType)
     val d = UpdateWidgetData(w.id, initialData)
 
@@ -47,16 +42,13 @@ object SPWidget {
 
 
 object SPWidgetBaseTest {
-  import sp.domain.Logic._
   def apply() = SPWidget{spwb =>
     def saveOnChange(e: ReactEventFromInput): Callback =
-      Callback(spwb.updateWidgetData(SPValue(e.target.value)))
+      Callback.log("Save On Change in SPWidgetBaseTest")
 
     def copyMe(): Callback = {
       val d = spwb.getWidgetData
-      Callback(spwb.openNewWidget(
-        "SPWBTest", d)
-      )
+      Callback.log("Copy me in SPWidgetBaseTest")
     }
 
 
@@ -65,7 +57,7 @@ object SPWidgetBaseTest {
       <.label("My Data"),
       <.input(
         ^.tpe := "text",
-        ^.defaultValue := spwb.getWidgetData.toJson,
+        ^.defaultValue := spwb.getWidgetData,
         ^.onChange ==> saveOnChange
       ),
       <.button("Copy me", ^.onClick --> copyMe()),
